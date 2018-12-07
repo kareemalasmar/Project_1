9da0d8952b2916aefffe4f4a74deb7f084310ae5
KA <kareemalasmar@gmail.com>
2018-12-07 13:44:47 -0800

    embed youtube videos

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,11 +1,14 @@
-console.log("hello")
 
 var youtubeKey = "AIzaSyBxpyfAxI6oQ0SmlVVG1RLx8ArXQGYpLyY";
-var youtubeSearch = "cats";
+var youtubeSearch = "hydroflask";
 
 $.ajax({
-    url: " https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=" + youtubeSearch + " review" + "&key="+ youtubeKey,          
+    url: " https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&videoEmbeddable=true&maxResults=10&q=" + youtubeSearch + " review" + "&key="+ youtubeKey,          
     method: "GET"
 }).then(function(data) {
-    console.log(data);
+    videoId = data.items[5].id.videoId;
+    console.log(videoId);
+    var video = $("<iframe>").attr({width: "560", height: "315", src: "https://www.youtube.com/embed/" + videoId, frameborder: "0", allow: "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture", allowfullscreen: "allowfullscreen"});
+    $("#videoContainer").append(video);
 });
+
