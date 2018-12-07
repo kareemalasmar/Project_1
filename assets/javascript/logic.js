9421932a6910a6084cd3b1f36b567922dc69b086
KA <kareemalasmar@gmail.com>
2018-12-07 13:00:50 -0800

    set up ajax url for youtube api

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,1 +1,11 @@
 console.log("hello")
+
+var youtubeKey = "AIzaSyBxpyfAxI6oQ0SmlVVG1RLx8ArXQGYpLyY";
+var youtubeSearch = "cats";
+
+$.ajax({
+    url: " https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=" + youtubeSearch + " review" + "&key="+ youtubeKey,          
+    method: "GET"
+}).then(function(data) {
+    console.log(data);
+});
